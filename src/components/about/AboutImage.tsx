dfe534d40d35e15c02bc68a964c18d39c9e0a4fe
Geetesh Dalal <geetesh472@gmail.com>
2026-09-18 01:01:07 +0530

    add profile image to about section

---
+++ src/components/about/AboutImage.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Image from "next/image";
+import profilepic from "@/assets/profilepic.avif";
+
+const AboutImage = () => {
+  return (
+    <div className="shrink-0 relative h-50 w-50 overflow-hidden rounded-sm border-2 border-primary shadow-[0_0_25px_var(--color-primary)]">
+      {" "}
+      <Image
+        src={profilepic}
+        alt="Geetesh Dalal"
+        className="object-cover"
+        fill
+      />
+    </div>
+  );
+};
+
+export default AboutImage;
