fd239df4ec6d045ad5f791e81622b5e8d3796178
Geetesh Dalal <geetesh472@gmail.com>
2026-09-18 17:08:18 +0530

    add tech stack and fix about section

--- src/components/about/EducationDetails.tsx
+++ src/components/about/EducationDetails.tsx
@@ -4,7 +4,7 @@ import bennettLogo from "@/assets/bennett.png";
 
 const EducationDetails = () => {
   return (
-    <div className="flex items-center gap-4 mt-4">
+    <div className="flex items-center gap-4 mt-2 sm:mt-4">
       {/* Bennett Logo */}
       <div className="shrink-0">
         <Image
@@ -16,7 +16,7 @@ const EducationDetails = () => {
       </div>
 
       {/* Education Details */}
-      <div className="flex flex-col gap-px font-body">
+      <div className="flex flex-col gap-px text-sm font-body">
         <span>Bennett University · 2023-2027</span>
         <span>B.Tech CSE</span>
         <span>8.88 CGPA</span>
